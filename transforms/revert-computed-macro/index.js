bbc977fa7332474f8ac4821703877530ee558f62
Eric White <ewhite@ciena.com>
2024-01-10 14:34:09 -0500

    Begin new transform

---
+++ transforms/revert-computed-macro/index.js
@@ -0,0 +1,42 @@
+const { getParser } = require('codemod-cli').jscodeshift;
+
+module.exports = function transformer(file, api) {
+  const j = getParser(api);
+  //const options = getOptions();
+
+  const root = j(file.source);
+
+  replaceEmberComputedImport(root, j);
+};
+
+function replaceEmberComputedImport(root, j) {
+  // Find the import declaration for 'ember-macro-helpers/computed'
+  const computedImport = root.find(j.ImportDeclaration, {
+    source: { value: 'ember-macro-helpers/computed' },
+  });
+
+  if (computedImport.size() > 0) {
+    // Remove the old import
+    computedImport.remove();
+
+    // Find the import declaration for '@ember/object'
+    const emberObjectImport = root.find(j.ImportDeclaration, {
+      source: { value: '@ember/object' },
+    });
+
+    if (emberObjectImport.size() > 0) {
+      // If '@ember/object' is already being imported, add 'computed' to the import specifiers
+      emberObjectImport.get('specifiers').push(j.importSpecifier(j.identifier('computed')));
+    } else {
+      // If '@ember/object' is not already being imported, add a new import declaration
+      root
+        .get('body')
+        .unshift(
+          j.importDeclaration(
+            [j.importSpecifier(j.identifier('computed'))],
+            j.literal('@ember/object')
+          )
+        );
+    }
+  }
+}
